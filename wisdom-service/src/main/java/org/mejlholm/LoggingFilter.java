dda93dca69952fc4940973a70c30e9a8492edb07
mejlholm <mejlholm@mejlholm.org>
2019-09-23 21:15:49 +0200

    Set uuid

--- wisdom-service/src/main/java/org/mejlholm/LoggingFilter.java
+++ wisdom-service/src/main/java/org/mejlholm/LoggingFilter.java
@@ -8,13 +8,16 @@ import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.ext.Provider;
+import java.util.UUID;
 
 
 @Provider
-public class LoggingFilter implements ContainerRequestFilter {
+public class LoggingFilter implements ContainerRequestFilter, ContainerResponseFilter {
 
     private static final Logger LOG = Logger.getLogger(LoggingFilter.class);
 
@@ -34,8 +37,19 @@ public class LoggingFilter implements ContainerRequestFilter {
         final String path = info.getPath();
         final String address = request.getRemoteAddr();
 
-        LOG.infof("Request %s %s from IP %s", method, path, address);
+        String uuid = tracer.activeSpan().getBaggageItem("uuid");
+        if (uuid == null) {
+            uuid = UUID.randomUUID().toString();
+            tracer.activeSpan().setBaggageItem("uuid", uuid);
+        }
 
-        tracer.activeSpan().setTag("method", method).setTag("path", path).setTag("address", address);
+        LOG.infof("Request %s %s %s from IP %s", uuid, method, path, address);
+
+        tracer.activeSpan().setTag("uuid", uuid).setTag("method", method).setTag("path", path).setTag("address", address);
+    }
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
+        LOG.infof("Response %s code %s ", tracer.activeSpan().getBaggageItem("uuid"), responseContext.getStatus());
     }
 }
