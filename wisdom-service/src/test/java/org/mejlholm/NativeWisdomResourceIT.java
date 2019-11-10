7f897f9e0a0ee3b93b83eb412de09cdf11363761
mejlholm <mejlholm@mejlholm.org>
2019-11-10 11:08:36 +0100

    Minor cleanup to make it build.

--- wisdom-service/src/test/java/org/mejlholm/NativeWisdomResourceIT.java
+++
@@ -1,9 +0,0 @@
-package org.mejlholm;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class NativeWisdomResourceIT extends WisdomResourceTest {
-
-    // Execute the same tests but in native mode.
-}
